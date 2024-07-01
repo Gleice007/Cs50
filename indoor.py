41a6da765c346c47790d4a1b5cb9e598658eb86a
Gleiciele <gleicielerodrigues57@gmail.com>
2024-07-01 11:23:00 -0300

    indoor

---
+++ indoor.py
@@ -0,0 +1,12 @@
+def main():
+    # Solicita ao usuário que insira uma entrada
+    input_str = input("Digite algo: ")
+
+    # Converte a entrada para letras minúsculas
+    lowercased_str = input_str.lower()
+
+    # Imprime a entrada convertida em minúsculas
+    print(lowercased_str)
+
+if __name__ == "__main__":
+    main()
